0be61405a71110fdb025632054c99d6ee670fd0b
micstudios <apps@micstudios.de>
2024-03-05 21:58:02 +0100

    minor changes

--- PrismDMX/sACN/sACN.swift
+++ PrismDMX/sACN/sACN.swift
@@ -2,7 +2,41 @@
 //  sACN.swift
 //  PrismDMX
 //
-//  Created by Christian Savage on 05.03.24.
+//  Created by simpelMIC on 05.03.24.
 //
 
 import Foundation
+import SwiftUI
+import Network
+import sACN
+
+class sACNController {
+    @Binding var universes: [Universe]
+    @State var isStreaming: Bool = false
+    
+    init(universes: Binding<[Universe]>) {
+        self._universes = universes
+    }
+    
+    func timer() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1/50) {
+            if self.isStreaming == true {
+                self.timer()
+                self.sendDMXData()
+            }
+        }
+    }
+    
+    func start() {
+        isStreaming = true
+        timer()
+    }
+    
+    func stop() {
+        isStreaming = false
+    }
+    
+    func sendDMXData() {
+        
+    }
+}
