7235a21da5e06e4e893bfd39388447edbbc2594c
simpelMIC <mic-pro@online.de>
2025-02-06 17:52:07 +0100

    Added install_pmx.sh

---
+++ Installation/install_pmx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install required packages
+echo "Installing required packages..."
+sudo apt update
+sudo apt install -y git
+
+echo "Downloading program files..."
+# wget -qO- https://github.com/simpelMIC/PrismDMX/archive/refs/heads/main.zip | bsdtar -xvf- --strip-components=1
+
+git clone https://github.com/simpelMIC/PrismDMX.git temp
+mv temp/* temp/.* .
+rm -rf temp
+
+# Run the other script (replace with the correct path to the script)
+echo "Installing swift..."
+chmod +x install_swift.sh
+chmod +x run.sh
+chmod +x build.sh
+chmod +x install_required_packages.sh
+
+./install_required_packages.sh
+
+./install_swift.sh
+
+./build.sh
+
+echo "Installation done..."
+
+read -p "Would you like to launch PrismDMX? (y/n): " LAUNCH
+
+if [[ "$LAUNCH" == "y" ]]; then
+echo "Launching..."
+./run.sh
+else
+echo "Done"
+fi
